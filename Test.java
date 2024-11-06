258a3361fd595c2f2677eaf0d0a678e137e1f471
Tu Dinh <tu.dinh@metropolia.fi>
2024-11-06 09:45:26 +0200

    test 7

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Just testing 5");
         System.out.println("Just testing 6");
+        System.out.println("Just testing 7");
     }
 }
